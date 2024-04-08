8bbe287b4001781a6f8c335d006692659831f77c
Ruthramoorthy26 <ruthramd007@gmail.com>
2024-04-08 16:20:46 +0530

    Project share to remote

---
+++ ASCIIValuesInJava/src/ascii/com/ASCIIValues.java
@@ -0,0 +1,13 @@
+package ascii.com;
+
+public class ASCIIValues {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 255; i++) {
+			System.out.println(i + " - " + (char) i);
+		}
+
+	}
+
+}
